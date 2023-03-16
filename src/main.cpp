f0eddd0844a293aafd464b72c25b7aa570a68544
joe-l-mathew <84284015+joe-l-mathew@users.noreply.github.com>
2023-03-16 12:55:35 +0530

    Completed basic working

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,11 @@ LiquidCrystal lcd(10, 11, 12, 13, 14, 15); // rs,en,data pins d4 -d7
 int motorPort = 16; // Motor Pin
 int motorSpeed = 0; // Motor Value- Initially Zero
 
-int a = 0;
-int b = 0;
-int c = 0;
-int d = 0;
+String encodedVal = "";
 
-int ac = 0;
-int aa = 0;
-int bb = 0;
-int cc = 0;
-int dd = 0;
-int ee = 0;
-
-const int incPort = 2; // increment switch
+const int incPort = 3; // increment switch
 int SW1INC = 1;
-const int decPort = 3; // Decrement Switch
+const int decPort = 2; // Decrement Switch
 int SW2DEC = 1;
 
 // Encoder input ports
@@ -30,25 +20,22 @@ const int S2E = 7;
 const int S3E = 8;
 const int S4E = 9;
 
-int S11E = 0;
-int S12E = 0;
-int S13E = 0;
-int S14E = 0;
-
-int S1Ealert = 1;
-int S2Ealert = 1;
-int S3Ealert = 1;
-int S4Ealert = 1;
+// Initial Encoded val
+int S11E = 1;
+int S12E = 1;
+int S13E = 1;
+int S14E = 1;
+int speed = 0;
+int speedLimit = 100;
 
 void setup()
 {
 
   lcd.begin(16, 2);
-  // setting motor speed to zero
-  analogWrite(motorPort, motorSpeed);
+  analogWrite(motorPort, motorSpeed); // setting motor speed to zero
   // Declaring switches
-  pinMode(incPort, INPUT);
-  pinMode(decPort, INPUT);
+  pinMode(incPort, INPUT_PULLUP);
+  pinMode(decPort, INPUT_PULLUP);
   // setting input port for encoder
   pinMode(S1E, INPUT);
   pinMode(S2E, INPUT);
@@ -62,378 +49,85 @@ void setup()
   lcd.print("Limit Controller");
   delay(5000);
   lcd.clear();
+  Serial.begin(9600);
 }
 
 void loop()
 {
+  lcd.setCursor(0, 0);
+  lcd.print("Speed: "+String(speed)+"       "); //printing speed on display
 
+//giving voltage to motor
+  analogWrite(16, speed * 255 / 100);
+
+  // increasing speed
+  if (digitalRead(incPort))
+  {
+    //checking for speed is lessthan 100
+    if(speed<=100){
+      speed += 10; //increasing by 10
+    }
+  }
+  //decreasing speed on button press and speed is greater than 0
+  if (digitalRead(decPort)&&speed>0)
+  {
+    speed -= 10; //decreasing by 10
+  }
+  // port S1E Status- HIGH/LOW
   S11E = digitalRead(S1E);
-  if (S1Ealert == LOW)
+  // port S2E Status- HIGH/LOW
+  S12E = digitalRead(S2E);
+  // port S3E Status- HIGH/LOW
+  S13E = digitalRead(S3E);
+  // port S4E Status- HIGH/LOW
+  S14E = digitalRead(S4E);
+  // asigning the encoded binary to variable
+  encodedVal = String(S11E) + String(S12E) + String(S13E) + String(S14E);
+  if (encodedVal == "1111")
   {
-    S11E = 0;
+    { // speed 100
+      speedLimit = 100;
+    }
+  };
+  if (encodedVal == "1110")
+  { // speed 20
+    speedLimit = 20;
   }
-  else
-  {
-    S11E = 1;
+  if (encodedVal == "1101")
+  { // speed 40
+    speedLimit = 40;
   }
-  S2Ealert = digitalRead(S2E);
-  if (S2Ealert == LOW)
-  {
-    S12E = 0;
+  if (encodedVal == "1011")
+  { // speed 60
+    speedLimit = 60;
   }
-  else
-  {
-    S12E = 1;
-  }
-  S3Ealert = digitalRead(S3E);
-  if (S3Ealert == LOW)
-  {
-    S13E = 0;
-  }
-  else
-  {
-    S13E = 1;
-  }
-  S4Ealert = digitalRead(S4E);
-  if (S4Ealert == LOW)
-  {
-    S14E = 0;
-  }
-  else
-  {
-    S14E = 1;
+  if (encodedVal == "0111")
+  { // speed 80
+    speedLimit = 80;
   }
 
-  /////////////////////////////////////////NORMAL NO AMBULANCE//////////////////////////////////////////////////////////////////////////////////
-  if ((S11E == 1) & (S12E == 1) & (S13E == 1) & (S14E == 1))
+  // speed limiting condition checking
+  if (speed > speedLimit)
   {
-    SW1INC = digitalRead(incPort);
-    if (SW1INC == LOW)
-    {
-      ac = ac + 1;
-      delay(500);
-      if (ac > 10)
-      {
-        ac = 10;
-        delay(500);
-      }
-    }
+    //Overspeed displaying
+    lcd.setCursor(0, 0);
+  lcd.print("OVERSPEED       ");
+  lcd.setCursor(0, 1);
+  // lcd.print("DETECTED    ");
+  delay(500);
+  
 
-    SW2DEC = digitalRead(decPort);
-    if (SW2DEC == LOW)
+    // for loop for gradual decrease of speed
+    for (int i = speed; i > speedLimit; i--)
     {
-      if (ac > 0)
-      {
-        ac = ac - 1;
-        delay(500);
-      }
+      analogWrite(16, i * 255 / 100);
+      delay(30);
     }
-    //-------------------------------Input-------------------------------------
-    if (ac == 0)
-    {
-      motorSpeed = 0;
-      analogWrite(motorPort, motorSpeed);
-      delay(500);
-      lcd.setCursor(0, 0);
-      lcd.print("Speed: 0 mph   ");
-      a = 0;
-      b = 0;
-      c = 0;
-      d = 0;
-    }
-    if (ac == 1)
-    {
-      motorSpeed = 100;
-      analogWrite(motorPort, motorSpeed);
-      delay(500);
-      lcd.setCursor(0, 0);
-      lcd.print("Speed: 10 mph   ");
-      a = 0;
-      b = 0;
-      c = 0;
-      d = 0;
-    }
-    if (ac == 2)
-    {
-      motorSpeed = 120;
-      analogWrite(motorPort, motorSpeed);
-      delay(500);
-      lcd.setCursor(0, 0);
-      lcd.print("Speed: 20 mph   ");
-      dd = 0;
-      a = 0;
-      b = 0;
-      c = 0;
-      d = 0;
-    }
-    if (ac == 3)
-    {
-      motorSpeed = 140;
-      analogWrite(motorPort, motorSpeed);
-      delay(500);
-      lcd.setCursor(0, 0);
-      lcd.print("Speed: 30 mph   ");
-      a = 1;
-      b = 0;
-      c = 0;
-      d = 0;
-    }
-    if (ac == 4)
-    {
-      motorSpeed = 160;
-      analogWrite(motorPort, motorSpeed);
-      delay(500);
-      lcd.setCursor(0, 0);
-      lcd.print("Speed: 40 mph   ");
-      cc = 0;
-      a = 1;
-      b = 0;
-      c = 0;
-      d = 0;
-    }
-    if (ac == 5)
-    {
-      motorSpeed = 180;
-      analogWrite(motorPort, motorSpeed);
-      delay(500);
-      lcd.setCursor(0, 0);
-      lcd.print("Speed: 50 mph   ");
-      a = 1;
-      b = 1;
-      c = 0;
-      d = 0;
-    }
-    if (ac == 6)
-    {
-      motorSpeed = 200;
-      analogWrite(motorPort, motorSpeed);
-      delay(500);
-      lcd.setCursor(0, 0);
-      lcd.print("Speed: 60 mph   ");
-      bb = 0;
-      a = 1;
-      b = 1;
-      c = 0;
-      d = 0;
-    }
-    if (ac == 7)
-    {
-      motorSpeed = 225;
-      analogWrite(motorPort, motorSpeed);
-      delay(500);
-      lcd.setCursor(0, 0);
-      lcd.print("Speed: 70 mph   ");
-      a = 1;
-      b = 1;
-      c = 1;
-      d = 0;
-    }
-    if (ac == 8)
-    {
-      motorSpeed = 235;
-      analogWrite(motorPort, motorSpeed);
-      delay(500);
-      lcd.setCursor(0, 0);
-      lcd.print("Speed: 80 mph   ");
-      ee = 0;
-      aa = 0;
-      a = 1;
-      b = 1;
-      c = 1;
-      d = 0;
-    }
-    if (ac == 9)
-    {
-      motorSpeed = 245;
-      analogWrite(motorPort, motorSpeed);
-      delay(500);
-      lcd.setCursor(0, 0);
-      lcd.print("Speed: 90 mph   ");
-      a = 1;
-      b = 1;
-      c = 1;
-      d = 1;
-    }
-    if (ac == 10)
-    {
-      ee = ee + 1;
-      if (ee == 1)
-      {
-        motorSpeed = 255;
-        analogWrite(motorPort, motorSpeed);
-        lcd.setCursor(0, 0);
-        lcd.print("Speed: 100 mph  ");
-        lcd.setCursor(0, 1);
-        lcd.print("Max speed       ");
-        delay(2000);
-        lcd.clear();
-      }
-
-      if (ee == 2)
-      {
-        motorSpeed = 255;
-        analogWrite(motorPort, motorSpeed);
-        lcd.setCursor(0, 0);
-        lcd.print("Speed: 100 mph  ");
-      }
-      if (ee == 3)
-      {
-        ee = 2;
-        a = 1;
-        b = 1;
-        c = 1;
-        d = 1;
-        goto st;
-      }
-    }
+    // assigning limited speed to speed
+    speed = speedLimit;
+    
   }
-st:
-  //////////////////////////////////////////vehicle speed 80mph/////////////////////////////////////////////////////////
-  if (d == 1)
-  {
-    if ((S11E == 0) & (S12E == 1) & (S13E == 1) & (S14E == 1))
-    {
-      aa = aa + 1;
-      if (aa == 1)
-      {
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Over Speeding   ");
-        lcd.setCursor(0, 1);
-        lcd.print("   Detected!!   ");
-        delay(5000);
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Capping Speed   ");
-        lcd.setCursor(0, 1);
-        lcd.print("   to 80mph     ");
-        delay(5000);
-        lcd.clear();
-      }
-      if (aa >= 2)
-      {
-        lcd.setCursor(0, 0);
-        lcd.print("SpeedLimit:80mph");
-        lcd.setCursor(0, 1);
-        lcd.print("Speed: 80 mph   ");
-        motorSpeed = 235;
-        analogWrite(motorPort, motorSpeed);
-        delay(500);
-        delay(3000);
-        lcd.clear();
-        ac = 8;
-      }
-    }
-  }
-  //////////////////////////////////////////vehicle speed 60mph/////////////////////////////////////////////////////////
-  if (c == 1)
-  {
-    if ((S11E == 1) & (S12E == 0) & (S13E == 1) & (S14E == 1))
-    {
-      bb = bb + 1;
-      if (bb == 1)
-      {
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Over Speeding   ");
-        lcd.setCursor(0, 1);
-        lcd.print("   Detected!!   ");
-        delay(5000);
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Capping Speed   ");
-        lcd.setCursor(0, 1);
-        lcd.print("   to 60mph     ");
-        delay(5000);
-        lcd.clear();
-      }
-      if (bb >= 2)
-      {
-        lcd.setCursor(0, 0);
-        lcd.print("SpeedLimit:60mph");
-        lcd.setCursor(0, 1);
-        lcd.print("Speed: 60 mph   ");
-        motorSpeed = 200;
-        analogWrite(motorPort, motorSpeed);
-        delay(500);
-        delay(3000);
-        lcd.clear();
-        ac = 6;
-      }
-    }
-  }
-  //////////////////////////////////////////vehicle speed 40mph/////////////////////////////////////////////////////////
-  if (b == 1)
-  {
-    if ((S11E == 1) & (S12E == 1) & (S13E == 0) & (S14E == 1))
-    {
-      cc = cc + 1;
-      if (cc == 1)
-      {
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Over Speeding   ");
-        lcd.setCursor(0, 1);
-        lcd.print("   Detected!!   ");
-        delay(5000);
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Capping Speed   ");
-        lcd.setCursor(0, 1);
-        lcd.print("   to 40mph     ");
-        delay(5000);
-        lcd.clear();
-      }
-      if (cc >= 2)
-      {
-        lcd.setCursor(0, 0);
-        lcd.print("SpeedLimit:40mph");
-        lcd.setCursor(0, 1);
-        lcd.print("Speed: 40 mph   ");
-        motorSpeed = 160;
-        analogWrite(motorPort, motorSpeed);
-        delay(500);
-        delay(3000);
-        lcd.clear();
-        ac = 4;
-      }
-    }
-  }
-  //////////////////////////////////////////vehicle speed 20mph////////////////////////////////////////////////////////
-  if (a == 1)
-  {
-    if ((S11E == 1) & (S12E == 1) & (S13E == 1) & (S14E == 0))
-    {
-      dd = dd + 1;
-      if (dd == 1)
-      {
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Over Speeding   ");
-        lcd.setCursor(0, 1);
-        lcd.print("   Detected!!   ");
-        delay(5000);
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Capping Speed   ");
-        lcd.setCursor(0, 1);
-        lcd.print("   to 20mph     ");
-        delay(5000);
-        lcd.clear();
-      }
-      if (dd >= 2)
-      {
-        lcd.setCursor(0, 0);
-        lcd.print("SpeedLimit:20mph");
-        lcd.setCursor(0, 1);
-        lcd.print("Speed: 20 mph   ");
-        motorSpeed = 120;
-        analogWrite(motorPort, motorSpeed);
-        delay(500);
-        delay(3000);
-        lcd.clear();
-        ac = 2;
-      }
-    }
-  }
+  
+  delay(300);
+  lcd.clear(); //clearing display
 }
